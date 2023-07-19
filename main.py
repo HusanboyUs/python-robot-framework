9d43bda48b5d2f24b3a16e2023e25e178b1c7931
Husanboy Usmonov <husanghost@gmail.com>
2023-07-19 12:25:50 +0200

    Pylint: static code analysis rate is updated to 10/10

--- main.py
+++ main.py
@@ -1,42 +1,63 @@
-#created my custom error in case of giving invalid inputs
+"""This is main task file which coder should write a script
+a class to add integers and return the result"""
+
+
 class CustomError(ValueError):
+    '''CustomError: Raised when the user provides non-integer input
+    Examples:
+        >>> raise CustomError("Invalid input! Only integers are allowed.")
+        CustomError: Invalid input! Only integers are allowed.
+    '''
+
     def __init__(self) -> str:
-        super().__init__(f"Inputs are not valid since task was to add only numbers not strings!")
+        super().__init__("Inputs are not valid since task was to add only numbers not strings!")
 
 
-class addNumbers:
+class AddNum:
+    '''Main task class to add two numbers
+
+    Methods:
+        add_nums(x, n):
+            Takes two integer arguments (x and n) and returns the sum of the two integers.
+            It also updates the result history with the computed sum.
+        
+        recent_result():
+            Returns the list of recent results, representing the history of computed sums.
+        
+        clean_result():
+            Resets the result history by clearing the list of computed sums.
+    '''
+
     def __init__(self):
-        self.result=[]
+        self.result = []
 
-    #main function
-    def addNums(self,x,n):
-        if isinstance(x,int) and isinstance(n,int):
-            result = n + x
+    # main function
+    def add_nums(self, val, val2):
+        '''Takes two arguments (integers) and returns sum of the both integers'''
+        if isinstance(val, int) and isinstance(val2, int):
+            result = val + val2
             self.result.append(result)
             return result
-        else:
-            raise CustomError()
+        raise CustomError()
 
-    #getting last/recent result result
+    # getting last/recent result result
     def recent_result(self):
+        '''Returns recent results of sums of integers'''
         return self.result
 
-    #resetting last result
+    # resetting last result
     def clean_result(self):
+        '''Cleans history result'''
         self.result = []
-        return 'Cleaned!'   
-    
+        return 'Cleaned!'
 
-myClass=addNumbers()
 
-print(myClass.addNums(55,4))
+myClass = AddNum()
+
+print(myClass.add_nums(10, 4))
 print(f'Recent result: {myClass.recent_result()}')
 
-print(myClass.addNums(323,4))
+
+print(myClass.add_nums(323, 4))
 print(f'Recent results: {myClass.recent_result()}')
 print(f'History is {myClass.clean_result()}')
-
-
-
-
-
--- tests.py
+++ tests.py
@@ -1,22 +1,41 @@
-from main import CustomError,addNumbers
-import pytest
+"""This  is a pytest script to check if main class (addNums) are woeking as expected"""
 
-test_class = addNumbers()
+import pytest
+from main import CustomError, AddNum
+
+
+test_class = AddNum()
+
+
 
 def test_numbers():
-    assert test_class.addNums(45,15) == 60
+    '''This test function checks if the sum_of_integers function correctly computes
+    the sum of two integers'''
+
+    assert test_class.add_nums(45, 15) == 60
+
 
 def test_negatives():
-    assert test_class.addNums(-40,5) == -35
+    '''This test function checks if the sum_of_integers function correctly computes
+    the sum of two negatives integers'''
+    assert test_class.add_nums(-40, 5) == -35
 
-def test_invalidInputs():
+
+def test_invalid_inputs():
+    '''This test function checks if the sum_of_integers function raises error when 
+    it gets unsupported characters like string'''
     with pytest.raises(CustomError):
-        test_class.addNums('string',5)
+        test_class.add_nums('string', 5)
+
 
 def test_recent():
-    add_new = test_class.addNums(23,5) #28 
+    '''This test function checks if the sum_of_integers function correctly addes recent results 
+    to history result and check if history result is correctly being returned with all 
+    recent history'''
+    first_calc = test_class.add_nums(23, 5)  # 28
+    print(first_calc)
     assert test_class.recent_result() == [60, -35, 28]
-    add_new = test_class.addNums(50,23) #73
-    assert test_class.recent_result() == [60, -35, 28,73] #hence we have added other numbers above
-    
-
+    add_new = test_class.add_nums(50, 23)  # 73
+    # hence we have added other numbers above
+    print(add_new)
+    assert test_class.recent_result() == [60, -35, 28, 73]
